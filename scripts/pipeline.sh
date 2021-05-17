4b5033307f82719f76af7ab5ce1df0829ca919ee
EC2 Default User <ec2-user@ip-172-31-34-32.us-east-2.compute.internal>
2021-05-17 03:22:36 +0000

    refactoring

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -38,7 +38,7 @@ logcmd(){
     "$@" > >(tee ${logbase}_stdout.txt) 2> >(tee ${logbase}_stderr.txt >&2) || error "logcmd $1"
 }
 
-version=alpha-1.0.2
+version=alpha-1.0.3
 
 # Calculate and store hash of this file for logging and reproducibility
 selfhash=$(sha256sum $0)
@@ -59,13 +59,16 @@ case "$FSLOUTPUTTYPE" in
 	;;
 esac
 
-#unset INDIR OUTDIR IMG FS_LICENSE
-usage="Usage: pipeline.sh [ -r SUBJ_RECON_DIR ] [ -o SUBJ_OUT_DIR ] [ -l FS_LICENSE ] [ -i IMG ] [ -c ]"
+#unset INDIR OUTDIR T1 FS_LICENSE
+usage="Usage: pipeline.sh [ -r SUBJ_RECON_DIR ] [ -o SUBJ_OUT_DIR ] [ -l FS_LICENSE ] [ -i T1 ] [ -j IMG2 ] [ -k IMG3 ]"
 
 # DEFAULT VALUES
-IMG="freesurfer_default"
+T1="freesurfer_default"
 CLEANUP_FLAG=0
-while getopts r:o:l:i:c arg; do
+CONTINUE_FLAG=0
+IMG2=0
+IMG3=0
+while getopts r:o:l:i:j:k:cx arg; do
     case "${arg}" in 
     r) INDIR="$OPTARG"
         ;;
@@ -73,9 +76,15 @@ while getopts r:o:l:i:c arg; do
         ;;
     l) export FS_LICENSE="$OPTARG"
         ;;
-    i) IMG="$OPTARG"
+    i) T1="$OPTARG"
+        ;;
+    j) IMG2="$OPTARG"
+        ;;
+    k) IMG3="$OPTARG"
         ;;
     c) CLEANUP_FLAG=1
+        ;;
+    x) CONTINUE_FLAG=1
         ;;
 	?) >&2 echo $usage
 	   exit 2
@@ -83,9 +92,9 @@ while getopts r:o:l:i:c arg; do
     esac
 done
 
-echo "Image input: ${IMG}"
+echo "Image input: ${T1}"
 
-if [ -d $OUTDIR ]; then
+if [ -d $OUTDIR ] && [ "${CONTINUE_FLAG}" = "0" ]; then
     error "ERROR: OUTDIR (-o) already exists ${OUTDIR}"
     #rm -rf $OUTDIR
 fi
@@ -107,15 +116,20 @@ check_file $FS_LICENSE
 #brainmask="${PIPELINE_HOME}"/models/icbm_mask_ref.nii.gz # Used for nornalization constant
 brainmask="${PIPELINE_HOME}"/models/MNI152_T1_2mm_bet_brain_mask.nii.gz # Used for nornalization constant
 atlas="${PIPELINE_HOME}"/models/atlas_labels_ref.nii.gz
+cerebrum_mask="${PIPELINE_HOME}"/models/cerebrum_mask.nii.gz
 mni_ref="${PIPELINE_HOME}"/models/MNI152_T1_2mm.nii.gz
 mni_ref_brain="${PIPELINE_HOME}"/models/MNI152_T1_2mm_brain.nii.gz # reference for registration
 fnirtconf="${PIPELINE_HOME}"/models/T1_2_MNI152_2mm.cnf
+REF_lobes=$PIPELINE_HOME/models/lobe_labels.csv
+REF_DK=$PIPELINE_HOME/models/aparc_aseg_labels.csv
 
 check_file $atlas
 check_file $brainmask
 check_file $mni_ref
 check_file $mni_ref_brain
 check_file $fnirtconf
+check_file $REF_lobes
+check_file $REF_DK
 
 mri_dir=mri
 mkdir -p $mri_dir
@@ -126,26 +140,26 @@ out_ext=$ext
 
 echo "CONVERTING ${in_ext} to ${out_ext}"
 # T1 Used for calculating stats
-if [ $IMG == "freesurfer_default" ]; then
-    # Freesurfer IMG
+if [ $T1 = "freesurfer_default" ]; then
+    # Freesurfer T1
     T1_N3_mgz="${INDIR}"/mri/nu$in_ext # orig_nu.mgz
     echo "Image not specified: Using default freesurfer (${T1_N3_mgz})"
-    IMG="${mri_dir}"/nu$out_ext
-    mri_convert $T1_N3_mgz $IMG
+    T1="${mri_dir}"/nu$out_ext
+    mri_convert $T1_N3_mgz $T1
     # Brain skull-stripped - T1 Used for FLIRT
     T1_brain_mgz="${INDIR}"/mri/brainmask$in_ext # orig_nu.mgz
     IMG_brain="${mri_dir}"/brain$out_ext
     mri_convert $T1_brain_mgz $IMG_brain
-elif [ -f $IMG ]; then
+elif [ -f $T1 ]; then
     bet_f=0.4  # parameter passed to FSL's  bet
     IMG_brain="${mri_dir}"/brain$out_ext
-    logcmd bet_log bet "$IMG" "$IMG_brain" -f "$bet_f" -R -s -m
+    logcmd bet_log bet "$T1" "$IMG_brain" -f "$bet_f" -R -s -m
 else
-    error "IMG file does not exist: ${IMG}"
+    error "T1 file does not exist: ${T1}"
 fi
 
 # Double check that files and directories exist
-check_file $IMG
+check_file $T1
 check_file $IMG_brain
 
 
@@ -160,6 +174,10 @@ GM="${mri_dir}"/GM_mask_ctx"${out_ext}"
 mri_binarize --i $GM_mgz --o $GM --match 3 42
 check_file $GM
 
+DK_mgz="${INDIR}"/mri/aparc+aseg"${in_ext}"
+DK_atlas="${mri_dir}"/DK_atlas"${out_ext}"
+mri_convert $DK_mgz $DK_atlas
+check_file $DK_atlas
 
 # REGISTRATION
 reg_dir="${OUTDIR}/registration"
@@ -169,8 +187,12 @@ mkdir -p $reg_dir
 echo "FLIRT LINEAR REGISTRATION"
 s2raff="${reg_dir}"/"struct2mni_affine.mat"
 T1_atlas="${reg_dir}"/"T1_atlas_flirt${out_ext}"
-echo flirt -ref "${mni_ref_brain}" -in "${IMG_brain}" -out "${T1_atlas}" -omat "${s2raff}"
-logcmd flirt_log flirt -ref "${mni_ref_brain}" -in "${IMG_brain}" -out "${T1_atlas}" -omat "${s2raff}"
+if [ "${CONTINUE_FLAG}" = "0" ]; then
+    echo flirt -ref "${mni_ref_brain}" -in "${IMG_brain}" -out "${T1_atlas}" -omat "${s2raff}"
+    logcmd flirt_log flirt -ref "${mni_ref_brain}" -in "${IMG_brain}" -out "${T1_atlas}" -omat "${s2raff}"
+else
+    echo "CONTINUE_FLAG: ${CONTINUE_FLAG} .. skipping flirt registration steps"
+fi
 check_file $T1_atlas
 check_file $s2raff
 
@@ -181,31 +203,89 @@ check_file $s2raff
 #     Uses linear registration as initial transformation
 echo "FNIRT NON-LINEAR REGISTRATION"
 s2rwarp="${reg_dir}"/"struct2mni_warp${out_ext}"
-echo fnirt --in="${IMG}" --config="${fnirtconf}" --ref="${mni_ref}" --aff="${s2raff}" --cout="${s2rwarp}"
-logcmd fnirt_log fnirt --in="${IMG}" --config="${fnirtconf}" --ref="${mni_ref}" --aff="${s2raff}" --cout="${s2rwarp}"
+if [ "${CONTINUE_FLAG}" = "0" ]; then
+    echo fnirt --in="${T1}" --config="${fnirtconf}" --ref="${mni_ref}" --aff="${s2raff}" --cout="${s2rwarp}"
+    logcmd fnirt_log fnirt --in="${T1}" --config="${fnirtconf}" --ref="${mni_ref}" --aff="${s2raff}" --cout="${s2rwarp}"
+else
+    echo "CONTINUE_FLAG: ${CONTINUE_FLAG} .. skipping fnirt registration steps"
+fi
+check_file $s2rwarp
 
 # Warp from native to standard space to QC
 T1_atlas2="${reg_dir}"/"T1_atlas_fnirt${out_ext}"
-logcmd t1_2_ref_log applywarp --ref="${mni_ref}" --in="${IMG}" --out="${T1_atlas2}" --warp="${s2rwarp}"
+logcmd t1_2_ref_log applywarp --ref="${mni_ref}" --in="${T1}" --out="${T1_atlas2}" --warp="${s2rwarp}"
 
 # Calculate inverse transformation
 echo "CALCULATING INVERSE TRANSFORM"
 r2swarp="${reg_dir}"/"mni2struct_warp${out_ext}"
-logcmd inv_warp_log invwarp --ref="${IMG}" --warp="$s2rwarp" --out="$r2swarp"
+if [ "${CONTINUE_FLAG}" = "0" ]; then
+    logcmd inv_warp_log invwarp --ref="${T1}" --warp="$s2rwarp" --out="$r2swarp"
+else
+    echo "CONTINUE_FLAG: ${CONTINUE_FLAG} .. skipping invwarp registration steps"
+fi
+check_file $r2swarp
 
 
 # apply inverse transformation to labels and brainmask
 #    use nearest neighbor interpolation
 echo "APPLYING INVERSE TRANSFORM"
 atlas_native="${reg_dir}"/"atlas_native${out_ext}"
-logcmd atlas_2_native_log applywarp --ref="${IMG}" --in="${atlas}" --out="${atlas_native}" --warp="${r2swarp}" --interp=nn --datatype=int
+logcmd atlas_2_native_log applywarp --ref="${T1}" --in="${atlas}" --out="${atlas_native}" --warp="${r2swarp}" --interp=nn --datatype=int
+check_file $atlas_native
 
 brainmask_native="${reg_dir}"/"brain_mask_native${out_ext}"
-logcmd brainmask_2_native_log applywarp --ref="${IMG}" --in="${brainmask}" --out="${brainmask_native}" --warp="${r2swarp}" --interp=nn --datatype=int
+logcmd brainmask_2_native_log applywarp --ref="${T1}" --in="${brainmask}" --out="${brainmask_native}" --warp="${r2swarp}" --interp=nn --datatype=int
+check_file $brainmask_native
+
+cerebrum_mask_native="${reg_dir}"/"cerebrum_mask_native${out_ext}"
+logcmd cerebrum_mask_2_native_log applywarp --ref="${T1}" --in="${cerebrum_mask}" --out="${cerebrum_mask_native}" --warp="${r2swarp}" --interp=nn --datatype=int
+check_file $cerebrum_mask_native
 
 # Get Stats
 stats_dir="${OUTDIR}/stats"
-python3 $PIPELINE_HOME/scripts/calc_stats.py -i $IMG -WM $WM -GM $GM -LM $atlas_native -BM $brainmask_native -o $stats_dir |& tee logs/stats_log.txt
+
+python3 $PIPELINE_HOME/scripts/stats.py -i $T1 -WM $WM -GM $GM \
+    -CM $cerebrum_mask_native -SM $atlas_native -r $REF_lobes -o $stats_dir \
+    -BM $brainmask_native -t lobes
+
+python3 $PIPELINE_HOME/scripts/stats.py -i $T1 \
+    -CM $cerebrum_mask_native -SM $DK_atlas -r $REF_DK -o $stats_dir \
+    -BM $brainmask_native -t DK
+
+check_dir $stats_dir
+
+# Get Stats - IMG2 (optional)
+if [ "${IMG2}" != "0" ]; then
+    stats_dir="${OUTDIR}/stats2"
+    check_file $IMG2
+    python3 $PIPELINE_HOME/scripts/stats.py -i $IMG2 -WM $WM -GM $GM \
+        -CM $cerebrum_mask_native -SM $atlas_native -r $REF_lobes -o $stats_dir \
+        -BM $brainmask_native -t lobes
+
+    python3 $PIPELINE_HOME/scripts/stats.py -i $IMG2 \
+        -CM $cerebrum_mask_native -SM $DK_atlas -r $REF_DK -o $stats_dir \
+        -BM $brainmask_native -t DK
+
+    check_dir $stats_dir
+fi
+
+# Get Stats - IMG3 (optional)
+if [ "${IMG3}" != "0" ]; then
+    stats_dir="${OUTDIR}/stats3"
+    check_file $IMG3
+    python3 $PIPELINE_HOME/scripts/stats.py -i $IMG3 -WM $WM -GM $GM \
+        -CM $cerebrum_mask_native -SM $atlas_native -r $REF_lobes -o $stats_dir \
+        -BM $brainmask_native -t lobes
+
+    python3 $PIPELINE_HOME/scripts/stats.py -i $IMG3 \
+        -CM $cerebrum_mask_native -SM $DK_atlas -r $REF_DK -o $stats_dir \
+        -BM $brainmask_native -t DK
+
+    check_dir $stats_dir
+fi
+
+#python3 $PIPELINE_HOME/scripts/calc_stats.py -i $T1 -WM $WM \
+# -GM $GM -LM $atlas_native -BM $brainmask_native -o $stats_dir |& tee logs/stats_log.txt
 
 # Cleanup
 ERROR=0
@@ -222,7 +302,7 @@ do
 done
 
 echo "CLEANUP_FLAG: ${CLEANUP_FLAG}"
-if [ "${CLEANUP_FLAG}" == 1 ]; then
+if [ "${CLEANUP_FLAG}" = 1 ]; then
     echo "Cleaning workdir"
     rm -rf ${reg_dir}
     rm -rf ${mri_dir}
---
+++ scripts/stats.py
@@ -0,0 +1,351 @@
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, ArgumentError
+import sys
+import os
+from os.path import join, split
+import traceback
+import time
+import pandas as pd
+import SimpleITK as sitk
+from scipy import stats
+import numpy as np
+
+measures = ['mean', 'std', 'skew', 'kurtosis', 'min', 'max', 'nVox']
+
+
+def resample_mask2ref(mask, refImg):
+    resample = sitk.ResampleImageFilter()
+    resample.SetReferenceImage(refImg)
+    resample.SetDefaultPixelValue(0)
+    resample.SetInterpolator(sitk.sitkNearestNeighbor)
+    resample.AddCommand(sitk.sitkProgressEvent,
+                        lambda: sys.stdout.flush())
+    msk_resampled = resample.Execute(mask)
+    return msk_resampled
+
+
+def check_exist(*args):
+    for arg in args:
+        if arg is None:
+            continue
+        if not os.path.isfile(arg):
+            raise Exception(f"{arg} file does not exist")
+
+
+def img_compare(img1, img2, v=1):
+    round_tpl = lambda tpl, n: tuple(round(x, n) for x in tpl)
+    size_1 = img1.GetSize()
+    size_2 = img2.GetSize()
+    spacing_1 = img1.GetSpacing()
+    spacing_2 = img2.GetSpacing()
+    origin_1 = img1.GetOrigin()
+    origin_2 = img2.GetOrigin()
+    direction_1 = img1.GetDirection()
+    direction_2 = img2.GetDirection()
+
+    if v:
+        print('size: \n img 1 {} \n img 2 {}'.format(round_tpl(size_1, 6),
+                                                     round_tpl(size_2, 6)))
+        print('spacing: \n img 1 {} \n img 2 {}'.format(round_tpl(spacing_1, 6),
+                                                        round_tpl(spacing_2, 6)))
+        print('origin: \n img 1 {} \n img 2 {}'.format(round_tpl(origin_1, 6),
+                                                       round_tpl(origin_2, 6)))
+        print('direction: \n img 1 {} \n img 2 {}'.format(round_tpl(direction_1, 6),
+                                                          round_tpl(direction_2, 6)))
+    same_size = np.allclose(size_1, size_2), 
+    same_spacing = np.allclose(spacing_1, spacing_2)
+    same_origin = np.allclose(origin_1, origin_2)
+    same_direction = np.allclose(direction_1, direction_2)
+    same = same_size and same_spacing and same_origin and same_direction
+    if v:
+        print(f"equivalent: {same}")
+        if not same_size:
+            print("Size missmatch {} not same as {}".format(size_1, size_2))
+        if not same_spacing:
+            print("Spacing missmatch {} not same as {}".format(spacing_1, spacing_2))
+        if not same_origin:
+            print("Origin missmatch {} not same as {}".format(origin_1, origin_2))
+        if not same_direction:
+            print("Direction missmatch {} not same as {}".format(direction_1, direction_2))
+    return same
+
+
+def resample_mask2ref(mask, refImg):
+    resample = sitk.ResampleImageFilter()
+    resample.SetReferenceImage(refImg)
+    resample.SetDefaultPixelValue(0)
+    resample.SetInterpolator(sitk.sitkNearestNeighbor)
+    resample.AddCommand(sitk.sitkProgressEvent,
+                        lambda: sys.stdout.flush())
+    msk_resampled = resample.Execute(mask)
+    return msk_resampled
+
+
+def img_norm(img_arr):
+    img_arr_norm = img_arr - img_arr.min()
+    img_arr_norm = img_arr_norm / img_arr_norm.max()
+    assert(img_arr_norm.min() == 0)
+    assert(img_arr_norm.max() == 1)
+    return img_arr_norm
+
+
+def assert_is_binary_mask(msk_arr):
+    mini = msk_arr.min()
+    assert mini == 0, f"Min Error {mini}"
+    maxi = msk_arr.max()
+    assert maxi <= 1, f"Max Error {maxi}"
+    uniq = np.unique(msk_arr)
+    assert len(uniq) <= 2, f"Unique values error {uniq}"
+
+def resample_to_IMG(IMG, WM, GM, CM, SM, BM):
+    ref_img = IMG
+    resampled_masks = []
+    for msk in [WM, GM, CM, SM, BM]:
+        if msk is None:
+            resampled_masks.append(None)
+            continue
+        if not img_compare(msk, ref_img, v=True):
+            msk = resample_mask2ref(msk, ref_img)
+        resampled_masks.append(msk)
+    return tuple(resampled_masks)
+
+
+def load_data(IMG_path, WM_path, GM_path, CM_path, SM_path, BM_path):
+    IMG = sitk.ReadImage(IMG_path)
+    if WM_path is not None:
+        WM = sitk.ReadImage(WM_path)
+    else:
+        WM = None
+    if GM_path is not None:
+        GM = sitk.ReadImage(GM_path)
+    else:
+        GM = None
+    if CM_path is not None:
+        CM = sitk.ReadImage(CM_path)
+    else:
+        CM = None
+    SM = sitk.ReadImage(SM_path)
+    BM = sitk.ReadImage(BM_path)
+    return IMG, WM, GM, CM, SM, BM
+
+
+def get_arrays(IMG, WM, GM, CM, SM, BM):
+    IMG_arr = sitk.GetArrayFromImage(IMG)
+    if WM is not None:
+        WM_arr = sitk.GetArrayFromImage(WM)
+        assert_is_binary_mask(WM_arr)
+    else:
+        WM_arr = None
+    if GM is not None:
+        GM_arr = sitk.GetArrayFromImage(GM)
+        assert_is_binary_mask(GM_arr)
+    else:
+        GM_arr = None
+    if CM is not None:
+        CM_arr = sitk.GetArrayFromImage(CM)
+        assert_is_binary_mask(CM_arr)
+    else:
+        CM_arr = None
+    SM_arr = sitk.GetArrayFromImage(SM)
+    BM_arr = sitk.GetArrayFromImage(BM)
+    assert_is_binary_mask(BM_arr)
+    
+    return IMG_arr, WM_arr, GM_arr, CM_arr, SM_arr, BM_arr
+
+
+def check_img_consistent(IMG, WM, GM, SM, BM, v=0):
+    ref_img = IMG
+    for img in [WM, GM, SM, BM]:
+        if img is None:
+            continue
+        assert img_compare(ref_img, img, v=v)
+
+
+def get_cerebrum_mask(SM_arr, BM_arr, ref_df):
+    print(type(SM_arr), type(BM_arr), type(ref_df))
+    SM_arr2 = SM_arr * BM_arr
+    CM_arr = SM_arr2.copy()
+    if 'cerebrum' not in ref_df.columns:
+        return None
+    non_cerebrum_lbls = ref_df[~ref_df['cerebrum']].index.values
+    for lbl in non_cerebrum_lbls:
+        CM_arr[CM_arr == lbl] = 0
+    CM_arr = CM_arr > 0
+    return CM_arr, SM_arr2
+
+
+def get_masked_stats(img_array, mask_array):
+    if mask_array.sum() == 0:
+        return {m: np.NaN for m in measures}
+    masked_img = img_array[np.where(mask_array)]
+    summary_stats = stats.describe(masked_img.ravel())
+    nobs, minmax, mean, var, skew, kurtosis = summary_stats
+    stats_dict = {'mean': mean, 'min': minmax[0], 'nVox': nobs,
+                  'max': minmax[1], 'skew': skew, 'kurtosis': kurtosis}
+    stats_dict['std'] = np.sqrt(var)
+    return stats_dict
+
+
+def get_stats(IMG_arr, SM_arr, ref_df, BM_arr, CM_arr=None,
+              TM_arr=None, cerebrum_only=False, 
+              vox_vol=None, save_path=None):
+
+    if 'cerebrum' in ref_df.columns and cerebrum_only:
+        cerebrum_idx = ref_df['cerebrum']
+        labels = list(ref_df[cerebrum_idx].index.values)
+        lobes = list(ref_df[cerebrum_idx]['label_name'].values)
+        non_cerebrum_lbls = ref_df[~cerebrum_idx].index.values
+    else:
+        labels = list(ref_df.index.values)
+        lobes = list(ref_df['label_name'].values)
+        non_cerebrum_lbls = []
+        #assert np.alltrue(np.sort([0]+labels) == np.sort(np.unique(SM_arr))), "labels {}, atlas lbls {}".format(labels, np.unique(SM_arr))
+    
+    stats_df = pd.DataFrame(index=lobes, columns=measures)
+    if TM_arr is not None:
+        SM_arr = SM_arr * TM_arr
+    for lbl in labels:
+        if lbl in non_cerebrum_lbls:
+            continue
+        lobe = ref_df.loc[lbl]['label_name']
+        print('\t label {}, lobe {}'.format(lbl, lobe))
+        SM_mask_i = SM_arr == lbl
+        assert_is_binary_mask(SM_mask_i)
+        assert(IMG_arr.shape == SM_mask_i.shape)
+        stats_dict = get_masked_stats(IMG_arr, SM_mask_i)
+        for m in measures:
+            print('\t \t {}: {}'.format(m, stats_dict[m]))
+            stats_df.loc[lobe, m] = stats_dict[m]
+    # Cerebrum stats
+    if CM_arr is not None:
+        if TM_arr is not None:
+            CM_arr_ = CM_arr * TM_arr
+        else:
+            CM_arr_ = CM_arr
+        stats_dict = get_masked_stats(IMG_arr, CM_arr_)
+        print('\t Cerebrum')
+        for m in measures:
+            print('\t \t {}: {}'.format(m, stats_dict[m]))
+            stats_df.loc['Cerebrum', m] = stats_dict[m]
+    # Full Brain
+    if not cerebrum_only:
+        if TM_arr is not None:
+            BM_arr_ = BM_arr * TM_arr
+        else:
+            BM_arr_ = BM_arr
+        stats_dict = get_masked_stats(IMG_arr, BM_arr_)
+        print('\t Brain')
+        for m in measures:
+            print('\t \t {}: {}'.format(m, stats_dict[m]))
+            stats_df.loc['Brain', m] = stats_dict[m]
+        # volume
+        if vox_vol is not None:
+            stats_df['volume [mm^3]'] = vox_vol * stats_df['nVox']
+        # Save
+    stats_df.index.name = 'Region'
+    if save_path is not None:
+        stats_df.to_csv(save_path)
+
+
+def save_masks(ref_img, WM_arr, GM_arr, CM_arr, SM_arr2, BM_arr, outdir, tag):
+    if WM_arr is not None:
+        WM = sitk.GetImageFromArray(WM_arr)
+        WM.CopyInformation(ref_img)
+        sitk.WriteImage(WM, join(outdir, 'WhiteMatterMask.nii.gz'))
+
+    if GM_arr is not None:
+        GM = sitk.GetImageFromArray(GM_arr)
+        GM.CopyInformation(ref_img)
+        sitk.WriteImage(GM, join(outdir, 'GrayMatterMask.nii.gz'))
+
+    if CM_arr is not None:
+        CM = sitk.GetImageFromArray(CM_arr)
+        CM.CopyInformation(ref_img)
+        sitk.WriteImage(CM, join(outdir, 'CerebrumMask.nii.gz'))
+
+    SM = sitk.GetImageFromArray(SM_arr2) # Label / Segmentation Mask
+    SM.CopyInformation(ref_img)
+    sitk.WriteImage(SM, join(outdir, f"LabelMask_{tag}.nii.gz"))
+
+    BM = sitk.GetImageFromArray(BM_arr)
+    BM.CopyInformation(ref_img)
+    sitk.WriteImage(BM, join(outdir, 'BrainMask.nii.gz'))
+
+
+def main(IMG_path, WM_path, GM_path, CM_path, SM_path, BM_path, 
+         ref_file, outdir, tag, debug):
+    if not(os.path.isdir(outdir)):
+        os.makedirs(outdir)
+    check_exist(IMG_path, WM_path, GM_path, CM_path, SM_path, BM_path, ref_file)
+    IMG, WM, GM, CM, SM, BM = load_data(IMG_path, WM_path, GM_path, CM_path, SM_path, BM_path)
+    ref_df = pd.read_csv(ref_file, index_col=0)
+    WM, GM, CM, SM, BM = resample_to_IMG(IMG, WM, GM, CM, SM, BM)
+    check_img_consistent(IMG, WM, GM, CM, SM, BM)
+    IMG_arr, WM_arr, GM_arr, CM_arr, SM_arr, BM_arr = get_arrays(IMG, WM, GM, CM, SM, BM)
+    SM_arr2 = SM_arr * BM_arr # Refine segmentation mask - all labels inside brain
+    
+    spacing = IMG.GetSpacing()
+    vox_vol = np.prod(spacing)
+    print("\t GETTING GLOBAL STATS...")
+    get_stats(IMG_arr, SM_arr2, ref_df, BM_arr, CM_arr=CM_arr,
+              TM_arr=None, vox_vol=vox_vol, 
+              save_path=join(outdir, f"Global_stats_{tag}.csv"))
+    if WM_arr is not None:
+        print("\t GETTING WM STATS...")
+        get_stats(IMG_arr, SM_arr2, ref_df, BM_arr, CM_arr=CM_arr,
+                TM_arr=WM_arr, vox_vol=vox_vol,
+                save_path=join(outdir, f"WM_stats_{tag}.csv"))
+    if GM_arr is not None:
+        print("\t GETTING GM STATS...")
+        get_stats(IMG_arr, SM_arr2, ref_df, BM_arr, CM_arr=CM_arr,
+                TM_arr=GM_arr, vox_vol=vox_vol, cerebrum_only=True,
+                save_path=join(outdir, f"GM_stats_{tag}.csv"))
+    save_masks(IMG, WM_arr, GM_arr, CM_arr, SM_arr2, BM_arr, outdir, tag)
+
+
+def get_parser():
+    module_parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    module_parser.add_argument("-i", dest="I", type=str,
+                               help="Input image")
+    module_parser.add_argument("-WM", dest="WM", type=str, default=None,
+                               help="WM mask path")
+    module_parser.add_argument("-GM", dest="GM", type=str, default=None,
+                               help="GM mask path")
+    module_parser.add_argument("-CM", dest="CM", type=str, default=None,
+                               help="Cerebrum mask path")
+    module_parser.add_argument("-SM", dest="SM", type=str,
+                               help="Segmentation mask path (lobes/DK)")
+    module_parser.add_argument("-BM", dest="BM", type=str,
+                               help="Brain mask path")
+    module_parser.add_argument("-r", dest="ref", type=str,
+                               help="reference table for segmentation")
+    module_parser.add_argument("-o", dest="outdir", type=str,
+                               help="Output directory path")
+    module_parser.add_argument("-t", dest="tag", type=str,
+                               help="info tag")
+    module_parser.add_argument("-debug", dest="debug", type=int, default=0,
+                               help="DEBUG MODE [1 - ON, 0 - OFF (default: 0)]")
+    return module_parser
+
+
+if __name__ == "__main__":
+    t0 = time.time()
+    parser = get_parser()
+    try:
+        args = parser.parse_args()
+        main(args.I,
+             args.WM,
+             args.GM,
+             args.CM,
+             args.SM,
+             args.BM,
+             args.ref,
+             args.outdir,
+             args.tag,
+             args.debug)
+    except ArgumentError as arg_exception:
+        traceback.print_exc()
+    except Exception as exception:
+        traceback.print_exc()
+    dt = (time.time() - t0) / 60
+    print('done... python script runtime: {} min'.format(dt))
+    sys.exit()
